b4282c6b6f43529e4eb1c0e5b48f8634073335e2
Caioba24 <dipietroapg@gmail.com>
2022-11-22 20:25:58 -0300

    comki de maloka

---
+++ app/src/main/java/br/com/etecia/login_novo/LoginFragment.java
@@ -0,0 +1,21 @@
+package br.com.etecia.login_novo;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class LoginFragment extends Fragment {
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_login, container, false);
+        return v;
+    }
+}
