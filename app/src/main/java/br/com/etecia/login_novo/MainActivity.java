7ba8b6ae13c121afbdd23879257837228d81f558
Caioba24 <dipietroapg@gmail.com>
2022-11-29 21:50:11 -0300

    Arrumando o Tab ao arrastar e deixando minimalista e lindo

--- app/src/main/java/br/com/etecia/login_novo/MainActivity.java
+++ app/src/main/java/br/com/etecia/login_novo/MainActivity.java
@@ -39,6 +39,13 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
-
+            viewPager2.registerOnPageChangeCallback(new ViewPager2.OnPageChangeCallback() {
+                @Override
+                public void onPageSelected(int position) {
+                    super.onPageSelected(position);
+                    tabLayout.getTabAt(position).select();
+                }
+            });
     }
+
 }
